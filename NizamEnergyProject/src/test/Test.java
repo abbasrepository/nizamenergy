6d3eda0de1ca6b4b189592c5f1eb15a7f466a8be
NizamBijli <NizamBijli@NizamBijli-PC>
2017-07-06 11:20:04 +0500

    s

--- NizamEnergyProject/src/test/Test.java
+++ NizamEnergyProject/src/test/Test.java
@@ -6,7 +6,7 @@ public static void main(String args[]){
 	System.out.println("hello bashir!!!");
 }
 
-  public static void main(String[] args) {
+  public static void maasin(String[] args) {
 		System.out.println("Welcome to GitHub..... Glad to learn.");
 		System.out.println("Welcome to GitHub..... Glad to learn and this is second commit.");
 	}
