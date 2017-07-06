947ddc4fae98af5a7506655177751c3e39482976
Waseem <Waseem@Waseem-PC>
2017-07-06 11:09:05 +0500

    gfdfgd

--- NizamEnergyProject/src/test/Test.java
+++ NizamEnergyProject/src/test/Test.java
@@ -1,5 +1,9 @@
 package test;
 
 public class Test {
+	public static void main(String[] args) {
+		System.out.println("Welcome to GitHub..... Glad to learn.");
+		System.out.println("Welcome to GitHub..... Glad to learn and this is second commit.");
+	}
 
 }
