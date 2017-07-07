3b73acd74df8bea4577e9821b03d96e0f3de7920
Danish <Danish@Danish-PC>
2017-07-07 12:27:16 +0500

    my first coomit

---
+++ NizamEnergyProject/src/Tester/Hello.java
@@ -0,0 +1,10 @@
+package Tester;
+
+public class Hello {
+	
+	public static void insertCustomer(int customerId,String customerName,customerCnic,customerEmail,
+			customerAdress,customerCell){
+		
+	}
+
+}
